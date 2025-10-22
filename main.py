681d62b2d7e1c309f382de345d25fadd3acc3360
Reetik Basniwal <reetikbasniwal@gmail.com>
2025-10-22 22:50:51 +0530

    revert original

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from ast import arguments
 import asyncio
 import base64
 import json
 import websockets
 import os
 from dotenv import load_dotenv
-
 from pharmacy_functions import FUNCTION_MAP
 
 load_dotenv()
@@ -18,14 +16,15 @@ def sts_connect():
 
     sts_ws = websockets.connect(
         "wss://agent.deepgram.com/v1/agent/converse",
-        subprotocols=["token",api_key]
+        subprotocols=["token", api_key]
     )
-
     return sts_ws
+
 
 def load_config():
     with open("config.json", "r") as f:
         return json.load(f)
+
 
 async def handle_barge_in(decoded, twilio_ws, streamsid):
     if decoded["type"] == "UserStartedSpeaking":
@@ -35,14 +34,17 @@ async def handle_barge_in(decoded, twilio_ws, streamsid):
         }
         await twilio_ws.send(json.dumps(clear_message))
 
-async def execute_function_call(func_name, arguments):
+
+def execute_function_call(func_name, arguments):
     if func_name in FUNCTION_MAP:
         result = FUNCTION_MAP[func_name](**arguments)
-        print(f"Function call results: {result}")
-    else :
-        result = {"error": f"Unkown function: {func_name}"}
+        print(f"Function call result: {result}")
+        return result
+    else:
+        result = {"error": f"Unknown function: {func_name}"}
         print(result)
         return result
+
 
 def create_function_call_response(func_id, func_name, result):
     return {
@@ -50,13 +52,13 @@ def create_function_call_response(func_id, func_name, result):
         "id": func_id,
         "name": func_name,
         "content": json.dumps(result)
-
     }
+
 
 async def handle_function_call_request(decoded, sts_ws):
     try:
         for function_call in decoded["functions"]:
-            func_name = function_call["function_call"]
+            func_name = function_call["name"]
             func_id = function_call["id"]
             arguments = json.loads(function_call["arguments"])
 
@@ -68,21 +70,21 @@ async def handle_function_call_request(decoded, sts_ws):
             await sts_ws.send(json.dumps(function_result))
             print(f"Sent function result: {function_result}")
 
-
     except Exception as e:
-        print(f"Error in calling function: {e}")
+        print(f"Error calling function: {e}")
         error_result = create_function_call_response(
-            func_id if "func_id" in locals() else "unkown",
-            func_name if "func_name" in locals() else "unkown",
-            {"error": f"Function call failed with {str(e)}"},
+            func_id if "func_id" in locals() else "unknown",
+            func_name if "func_name" in locals() else "unknown",
+            {"error": f"Function call failed with: {str(e)}"}
         )
         await sts_ws.send(json.dumps(error_result))
+
 
 async def handle_text_message(decoded, twilio_ws, sts_ws, streamsid):
     await handle_barge_in(decoded, twilio_ws, streamsid)
 
     if decoded["type"] == "FunctionCallRequest":
-        await handle_function_call_request(decoded, sts_ws, )
+        await handle_function_call_request(decoded, sts_ws)
 
 
 async def sts_sender(sts_ws, audio_queue):
@@ -142,10 +144,8 @@ async def twilio_receiver(twilio_ws, audio_queue, streamsid_queue):
                 chunk = inbuffer[:BUFFER_SIZE]
                 audio_queue.put_nowait(chunk)
                 inbuffer = inbuffer[BUFFER_SIZE:]
-
         except:
             break
-
 
 
 async def twilio_handler(twilio_ws):
@@ -166,15 +166,12 @@ async def twilio_handler(twilio_ws):
 
         await twilio_ws.close()
 
+
 async def main():
     await websockets.serve(twilio_handler, "localhost", 5000)
     print("Started server.")
     await asyncio.Future()
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-
-
-
